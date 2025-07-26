b7517d7cd185d06f66e9a0f36902f142655ae8b5
MADHAV KALRA <68096582+madhavkalra7@users.noreply.github.com>
2025-07-26 22:09:29 +0530

    : AI JOBS

--- genetix/src/inngest/functions.ts
+++ genetix/src/inngest/functions.ts
@@ -1,10 +1,18 @@
 import { inngest } from "./client";
-
+import { createAgent, gemini } from "@inngest/agent-kit";
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event}) => {
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system: "You are an expert next js developer.  You write readable,maintainable code.You write simple next.js and react snippets.",
+      model: gemini({ model: 'gemini-2.0-flash'}),
+    });
+    const { output } = await codeAgent.run(
+  'write the following snippet' + "create a button component",
+);
+return {
+      summary: output,}
   },
 );
