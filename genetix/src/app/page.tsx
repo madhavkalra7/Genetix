f739c2409de531872bc97d6dd62cacfa505febda
MADHAV KALRA <68096582+madhavkalra7@users.noreply.github.com>
2025-07-26 22:09:29 +0530

    : AI JOBS

--- genetix/src/app/page.tsx
+++ genetix/src/app/page.tsx
@@ -1,10 +1,24 @@
 'use client';
-
-const Page= async ()=>{
-
+import {Input} from "@/components/ui/input";
+import { useState } from "react";
+import { inngest } from "@/inngest/client";
+const Page= ()=>{
+  const [value, setValue] = useState("");
 
   return(
    <div>
+    <h1>Genetix</h1>
+    <button
+      onClick={async () => {
+        await inngest.send({
+          name: 'test/hello.world',
+          data: { value },
+        });
+      }}
+    >
+      Send
+    </button>
+    <Input value={value} onChange={(e) => setValue(e.target.value)} />
     test
    </div>
   );
