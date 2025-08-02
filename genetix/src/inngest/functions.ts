8a228fb2f929ddd2dfb288feac695be98a9ed39d
MADHAV KALRA <68096582+madhavkalra7@users.noreply.github.com>
2025-08-02 23:17:05 +0530

    09 : projects

--- genetix/src/inngest/functions.ts
+++ genetix/src/inngest/functions.ts
@@ -149,6 +149,7 @@ export const codeAgentFunction= inngest.createFunction(
       if(isError){
         return await prisma.message.create({
           data: {
+            projectId: event.data.projectId,
             content: "Something Went Wrong. Please try again.",
             role: "ASSISTANT",
             type: "ERROR"
@@ -157,6 +158,7 @@ export const codeAgentFunction= inngest.createFunction(
       }
       return prisma.message.create({
         data: {
+          projectId: event.data.projectId,
           content: result.state.data.summary,
           role:"ASSISTANT",
           type: "RESULT",
