52b996c262f1c271260164f56d658fbb3c0c95f2
MADHAV KALRA <68096582+madhavkalra7@users.noreply.github.com>
2025-08-14 18:28:24 +0530

    11: project-header

--- genetix/src/modules/projects/ui/components/messages-container.tsx
+++ genetix/src/modules/projects/ui/components/messages-container.tsx
@@ -3,29 +3,44 @@ import { useSuspenseQuery } from "@tanstack/react-query";
 import { useTRPC } from "@/trpc/client";
 import { MessageCard } from "./message-card";
 import { MessageForm } from "./message-form";
-
+import { Fragment } from "@/generated/prisma";
+import { MessageLoading } from "./message-loading";
 interface Props {
     projectId: string;
+    activeFragment:Fragment | null;
+    setActiveFragment: (fragment:Fragment | null)=>void;
 }
-export const MessagesContainer = ({ projectId }: Props) => {
+export const MessagesContainer = ({ 
+    projectId,
+    activeFragment,
+    setActiveFragment
+
+}: Props) => {
     const bottomRef=useRef<HTMLDivElement>(null);
     const trpc = useTRPC();
     const { data: messages } = useSuspenseQuery(trpc.messages.getMany.queryOptions({
         projectId: projectId,
-    }));
+    },
+    {
+        refetchInterval:5000,
+    }
+));
 
     useEffect(() => {
-        const lastAssistantMessage=messages.findLast(
-            (message) => message.role==="ASSISTANT",
+        const lastAssistantMessageWithFragment=messages.findLast(
+            (message) => message.role==="ASSISTANT" && !!message.fragments,
         );
-        if(lastAssistantMessage){
-            // TODO SET ACTIVE FRAGMENT
+        if(lastAssistantMessageWithFragment){
+            setActiveFragment(lastAssistantMessageWithFragment.fragments);
         }
-    },[messages]);
+    },[messages,setActiveFragment]);
 
     useEffect(() => {
         bottomRef.current?.scrollIntoView();
     },[messages.length]);
+
+    const lastMessage=messages[messages.length-1];
+    const isLastMessageUser=lastMessage?.role==="USER";
 
     return(
         <div className="flex flex-col flex-1 min-h-0">
@@ -38,12 +53,13 @@ export const MessagesContainer = ({ projectId }: Props) => {
                             role={message.role}
                             fragment={message.fragments}
                             createdAt={message.createdAt}
-                            isActiveFragment={false}
-                            onFragmentClick={() => {}}
+                            isActiveFragment={activeFragment?.id===message.fragments?.id}
+                            onFragmentClick={() => setActiveFragment(message.fragments)}
                             type={message.type}
                             
                         />
                     ))}
+                    {isLastMessageUser && <MessageLoading/>}
                     <div ref={bottomRef}/>
                 </div>
             </div>
