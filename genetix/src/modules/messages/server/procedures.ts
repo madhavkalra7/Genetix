d41bf2ef7d8d4ba5f35f13593a6ba39cfca76713
MADHAV KALRA <68096582+madhavkalra7@users.noreply.github.com>
2025-08-14 09:53:04 +0530

    10: Messages-Ui

--- genetix/src/modules/messages/server/procedures.ts
+++ genetix/src/modules/messages/server/procedures.ts
@@ -2,10 +2,23 @@ import { prisma } from "@/lib/db";
 import { z } from "zod";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import { inngest } from "@/inngest/client";
+import { Input } from "@/components/ui/input";
 export const messagesRouter = createTRPCRouter({
     getMany: baseProcedure
-        .query(async ()=>{
+        .input(
+            z.object({
+                
+                projectId: z.string().min(1, { message: "Project ID is required" }),
+            }),
+        )
+        .query(async ({ input })=>{
             const messages= await prisma.message.findMany({
+                where: {
+                    projectId: input.projectId,
+                },
+                include: {
+                    fragments: true,
+                },
                 orderBy: {
                     updatedAt: "asc",
                 },
